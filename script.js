16a1febd12d9452fdcb55d1c620c8df4f1bd38eb
Shawn Edwards <sedwardsmm@gmail.com>
2020-02-08 17:53:27 -0600

    visual aesthetic improvements including respomnsive grid layout, new coloring, hover and selection color alteration, creation of random ai structure to choose from remaining selections randomly, currently starting work on linking this choice via the addMark method

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 let body = document.querySelector('body');
 let playerOnesTurn = true;
 let boardStateArray = [0,0,0,0,0,0,0,0,0];
+let opponentMode = "simpleAI";
+let boardStateObjects = document.getElementsByClassName("cell");
 
 const gameBoard = () => {
 
+    const countOpenSquares = () => {
+        let zeroesCounted = 0;
+        boardStateArray.forEach(function(item){
+            if (item == 0) {zeroesCounted++;}
+        })
+        return zeroesCounted;
+    }
+
+    const aiMove = () => {
+        switch (opponentMode) {
+            case "simpleAI":
+                let choice = Math.floor(Math.random() * countOpenSquares());
+                let counter = 0;
+                var result;
+                for (i = 0; i < boardStateArray.length; i++) {
+                    if (boardStateArray[i] == 0) {
+                    if (counter == choice) {result = i};
+                    counter++;
+                    }
+                }
+                return boardStateObjects[result];
+            break;
+            case "complexAI":
+
+            break;
+            case "human":
+
+            break;
+        }
+    }
+
+    const addMarkHandler = (e) => {
+
+    }
 
     //function to add XO to grid, change active player, assign graphic
     const addMark = (e) => {
-        console.log(e.target.className);
-        if(e.target.className == "gameSquare") {
-                e.target.className = "clicked";
+        if(e.target.className == "cell gameSquare") {
+                e.target.className = "cell clicked";
                 e.target.innerText = playerOnesTurn == true ? "X" : "O";
                 boardStateArray[e.target.getAttribute('id')-1] = playerOnesTurn == true ? 1 : -1;
                 playerOnesTurn = playerOnesTurn == true ? false : true;
-                console.log(e.target.getAttribute('id'));
-                console.log(boardStateArray);
+                //should now call appropriate AI function to fill next square or pass turn
+                //to the other human player
+                //if (playerOnesTurn == false) {addMark(aiMove())};
             }
     }
-    return {addMark};
-
-    
+    return {addMark, aiMove, countOpenSquares, addMarkHandler};    
 
 };
 
@@ -33,23 +67,22 @@ const players = () => {
 };
 
 const gameEngine = () => {
-    let winner = "";
 
-    //check horizonals
+    //simple AI to fill random square after player chooses square
+ 
+    //smart AI
+
+    let winner = ""; 
     const checkHorizontals = (arr) => {
         let result = 0;
-
         for (i = 1; i < boardStateArray.length+1; i++) {
             result += boardStateArray[i-1];
             if (result == 3) { winner = "Player One" };
             if (result == -3) { winner = "Player Two" };
             if (i % 3 === 0) {result = 0};
         }
-        console.log(winner);
     }
-    //check verticals
     const checkVerticals = (arr) => {
-   
     for (j = 1; j < 4; j++) {
         let result = 0;
         for (i = j; i < boardStateArray.length+1; i=i+3) {
@@ -58,25 +91,19 @@ const gameEngine = () => {
             if (result == -3) { winner = "Player Two" };
         }
     }
-    console.log(winner);
 }
-    //check diagonals
     const checkDiagonals = (arr) => {
         let diagOne = boardStateArray[0] + boardStateArray[4] + boardStateArray[8];
         let diagTwo = boardStateArray[2] + boardStateArray[4] + boardStateArray[6];
-        console.log("diagOne is" +diagOne);
-        console.log("diagTwo is " +diagTwo);
         if (diagOne == 3 || diagTwo == 3) { winner = "Player One" };
         if (diagOne == -3 || diagTwo == -3) { winner = "Player Two" };
-        console.log(winner);
     }
-
     return {checkHorizontals, checkVerticals, checkDiagonals};
 };
 
 let board = gameBoard();
 let engine = gameEngine();
-body.addEventListener('click', board.addMark);
+body.addEventListener('click', board.addMarkHandler);
 body.addEventListener('click', engine.checkHorizontals);
 body.addEventListener('click', engine.checkVerticals);
 body.addEventListener('click', engine.checkDiagonals);
