2e67d17afac5f3fb11e9986210b3a2f38ad9b95a
Shawn Edwards <sedwardsmm@gmail.com>
2020-02-13 05:46:44 -0600

    initial recurise ai framework in place, saving prior to refactoring the checkWinner portions to not explicitly call for boardStateArray but instead accept a generic array.  This will allow this same function to be used in the recursiveAI function

--- script.js
+++ script.js
@@ -2,69 +2,14 @@ let body = document.querySelector('body');
 let playerOnesTurn = true;
 let boardStateArray = [0,0,0,0,0,0,0,0,0];
 let opponentMode = "simpleAI";
+let difficulty = 9;
 let boardStateObjects = document.getElementsByClassName("cell");
-
-const gameBoard = () => {
-
-    const countOpenSquares = () => {
-        let zeroesCounted = 0;
-        boardStateArray.forEach(function(item){
-            if (item == 0) {zeroesCounted++;}
-        })
-        return zeroesCounted;
-    }
-
-    const aiMove = () => {
-        switch (opponentMode) {
-            case "simpleAI":
-                let choice = Math.floor(Math.random() * countOpenSquares());
-                let counter = 0;
-                var result;
-                for (i = 0; i < boardStateArray.length; i++) {
-                    if (boardStateArray[i] == 0) {
-                    if (counter == choice) {result = i};
-                    counter++;
-                    }
-                }
-                return boardStateObjects[result];
-            break;
-            case "complexAI":
-
-            break;
-            case "human":
-
-            break;
-        }
-    }
-
-    const addMarkHandler = (e) => {
-
-    }
-
-    //function to add XO to grid, change active player, assign graphic
-    const addMark = (e) => {
-        if(e.target.className == "cell gameSquare") {
-                e.target.className = "cell clicked";
-                e.target.innerText = playerOnesTurn == true ? "X" : "O";
-                boardStateArray[e.target.getAttribute('id')-1] = playerOnesTurn == true ? 1 : -1;
-                playerOnesTurn = playerOnesTurn == true ? false : true;
-                //should now call appropriate AI function to fill next square or pass turn
-                //to the other human player
-                //if (playerOnesTurn == false) {addMark(aiMove())};
-            }
-    }
-    return {addMark, aiMove, countOpenSquares, addMarkHandler};    
-
-};
-
-
-
-
-const players = () => {
-
-
-
-};
+let play = document.getElementById("play_button");
+let mainMenu = document.getElementById("main_menu");
+let displayMenu = true;
+let ticC = document.getElementsByClassName("tic_container")[0];
+let gameC = document.getElementsByClassName("game_container")[0];
+let mainC = document.getElementsByClassName("main_container")[0];
 
 const gameEngine = () => {
 
@@ -98,12 +43,137 @@ const gameEngine = () => {
         if (diagOne == 3 || diagTwo == 3) { winner = "Player One" };
         if (diagOne == -3 || diagTwo == -3) { winner = "Player Two" };
     }
+
+
     return {checkHorizontals, checkVerticals, checkDiagonals};
 };
+let engine = gameEngine();
+
+const checkWinner = () => {
+    engine.checkVerticals();
+    engine.checkHorizontals();
+    engine.checkDiagonals();
+    console.log("working");
+}
+
+const gameBoard = () => {
+
+    const changeScreen = () => {
+        displayMenu = !displayMenu;
+        if (displayMenu) {
+            ticC.style.display = "none";
+            gameC.style.display = "none";
+            mainC.style.display = "grid";
+        }
+        else {
+            ticC.style.display = "grid";
+            gameC.style.display = "grid";
+            mainC.style.display = "none";
+        }
+
+    }
+
+    const countOpenSquares = () => {
+        let zeroesCounted = 0;
+        boardStateArray.forEach(function(item){
+            if (item == 0) {zeroesCounted++;}
+        })
+        return zeroesCounted;
+    }
+
+    const aiMove = () => {
+        switch (opponentMode) {
+            case "simpleAI":
+                let choice = Math.floor(Math.random() * countOpenSquares());
+                let counter = 0;
+                var result;
+                for (i = 0; i < boardStateArray.length; i++) {
+                    if (boardStateArray[i] == 0) {
+                    if (counter == choice) {result = i};
+                    counter++;
+                    }
+                }
+                return boardStateObjects[result];
+            break;
+            case "complexAI":
+                const determinePlay = () => {
+
+                    //set initial value for which move to make to nonexistant cell 
+                    //and a boolean for ourTurn to false
+                    let move = -1;
+                    let score = -2;
+                    let ourTurn = false;
+                    let testBoardState = boardStateArray;
+
+                    //set ourTurn = !ourTurn
+                    //test each available move with a copied board, 
+                    //assigning a +1 value to any winning move if our turn, -1 if opp
+
+                    const recursiveAI = (boardState) => {
+
+                    ourTurn = !ourTurn;
+                    for (i = 0; i < 9; i++) {
+                        if (boardState[i] == 0) {
+
+                        }
+
+                    }
+                    //call this function again with the new boardstate
+
+                    //if winning move found, return this move
+
+
+                    //if no moves are available, return 0
+                    }
+
+                }
+
+            break;
+            case "human":
+
+            break;
+        }
+    }
+
+    
+
+    //function to add XO to grid, change active player, assign graphic
+    const addMark = (e) => {
+        if(e.className == "cell gameSquare") {
+                e.className = "cell clicked";
+                e.innerText = playerOnesTurn == true ? "X" : "O";
+                boardStateArray[e.getAttribute('id')-1] = playerOnesTurn == true ? 1 : -1;
+                playerOnesTurn = playerOnesTurn == true ? false : true;
+                //should now call appropriate AI function to fill next square or pass turn
+                //to the other human player
+                if (playerOnesTurn == false) {addMark(aiMove())};
+            }
+    }
+
+    const addMarkHandler = (e) => {
+        let domE = e.target;
+        console.log(domE);
+        addMark(domE);
+    }
+
+    return {addMark, aiMove, countOpenSquares, addMarkHandler, changeScreen};    
+
+};
+
+
+
+
+const players = () => {
+
+
+
+};
+
+
 
 let board = gameBoard();
-let engine = gameEngine();
+
 body.addEventListener('click', board.addMarkHandler);
-body.addEventListener('click', engine.checkHorizontals);
-body.addEventListener('click', engine.checkVerticals);
-body.addEventListener('click', engine.checkDiagonals);
+body.addEventListener('click', checkWinner);
+play.addEventListener('click', board.changeScreen);
+mainMenu.addEventListener('click', board.changeScreen);
